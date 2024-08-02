9aa56022e8ff0efbd29a5934d17cedbd3b776034
NDKha-DEV <dinhkha2406@gmail.com>
2024-08-02 20:20:47 +0700

    Update BookingController.cs

--- MVC/Controller/BookingController.cs
+++ MVC/Controller/BookingController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
@@ -11,6 +12,7 @@ public static class BookingController
 {
     public static void ManagementBooking()
     {
+
         while (true)
         {
             Menu.BookingMenu();
@@ -20,26 +22,27 @@ public static class BookingController
             switch (choice)
             {
                 case "1":
-                    AddBooking();
+                    CheckingRoomController.CheckingRoom();
+                    // AddBooking();
                     break;
                 case "2":
                     UpdateBooking();
                     break;
-                case "3":
-                    DeleteBooking();
-                    break;
+                // case "3":
+                //     DeleteBooking();
+                //     break;
                 // case "4":
                 //     ShowBookingList();
                 //     AnsiConsole.MarkupLine("[bold green]Press any key to go back![/]");
                 //     Console.ReadKey();
                 //     break;
-                case "4":
+                case "3":
                     SearchBookingByCustomerID();
                     break;
                 case "0":
                     Console.Clear();
                     return;
-                    break;
+                    
                 default:
                     AnsiConsole.MarkupLine("[bold red]Invalid choice! Please enter a valid choice![/]");
                     //Console.ReadKey();
@@ -48,14 +51,32 @@ public static class BookingController
         }
     }
 
-    private static void AddBooking()
+    public static void AddBooking()
     {
         using var db = new HotelContext();
         ConsoleUI uI = new ConsoleUI();
-        Console.Clear();
+        AnsiConsole.Clear();
         uI.Title("Add Booking");
         Booking booking = new Booking();
         int roomId;
+        int customerId;
+        // Get customer ID
+        uI.GreenMessage("Enter customer ID to add booking: ");
+        while (!int.TryParse(Console.ReadLine(), out customerId) || customerId <= 0)
+        {
+            AnsiConsole.MarkupLine("[underline red]Invalid input! Please enter a valid input![/]");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.White;
+            // Console.ReadKey();
+            uI.GreenMessage("Enter customer ID to add booking: ");
+        }
+        var customer = db.Customers.FirstOrDefault(r => r.CustomerId == customerId);
+        if (customer == null)
+        {
+            uI.RedMessage("Customer not found!");
+            CustomerController.SearchingCustomer(); 
+            AddBooking();
+        }        
         // Get room ID
         uI.GreenMessage("Enter room ID to add booking: ");
         while (!int.TryParse(Console.ReadLine(), out roomId) || roomId <= 0)
@@ -66,33 +87,18 @@ public static class BookingController
             // Console.ReadKey();
             uI.GreenMessage("Enter room ID to add booking: ");
         }
-        int customerId;
-        // Get customer ID
-        uI.GreenMessage("Enter customer ID: ");
-        while (!int.TryParse(Console.ReadLine(), out customerId) || customerId <= 0)
-        {
-            AnsiConsole.MarkupLine("[underline red]Invalid input! Please enter a valid input![/]");
-            Console.WriteLine();
-            Console.ForegroundColor = ConsoleColor.White;
-            // Console.ReadKey();
-            uI.GreenMessage("Enter customer ID: ");
-        }
-        var customer = db.Customers.FirstOrDefault(r => r.CustomerId == customerId);
-        if (customer == null)
-        {
-            uI.RedMessage("Customer not found!");
-            return;
-        }
+
+
         // Get room type
-        uI.GreenMessage("Enter room type: ");
+        uI.GreenMessage("Enter room type(Double/Quad): ");
         string roomtype = Console.ReadLine();
-        while (string.IsNullOrEmpty(roomtype))
+        while (string.IsNullOrEmpty(roomtype) || (roomtype != "Double" && roomtype != "Quad"))
         {
             AnsiConsole.MarkupLine("[underline red]Invalid input! Please enter a valid input![/]");
-            Console.WriteLine();
+            // Console.WriteLine();
             Console.ForegroundColor = ConsoleColor.White;
             // Console.ReadKey();
-            uI.GreenMessage("Enter room type: ");
+            uI.GreenMessage("Enter room type(Double/Quad): ");
             roomtype = Console.ReadLine();
         }
         
@@ -102,28 +108,40 @@ public static class BookingController
         do
         {
             Console.WriteLine();
-            var date_checkin = AnsiConsole.Ask<string>("[underline green]Enter check in date(YYYY/MM/DD): [/]");
+            var date_checkin = AnsiConsole.Ask<string>("[underline green]Enter check in date(dd/MM/yyyy): [/]");
             // Console.WriteLine();
             check_in_date = EnterDate(date_checkin);
             if (check_in_date == null)
             {
                 AnsiConsole.MarkupLine("[red]Invalid input! Please enter a valid input![/]");
+            } else if (check_in_date < DateTime.Today)
+            {
+                AnsiConsole.MarkupLine("[red]Check in date must be in the future![/]");
+                check_in_date = null;
             }
         } while (check_in_date == null);
         // Enter check out date
         do
         {
             Console.WriteLine();
-            var date_checkout = AnsiConsole.Ask<string>("[underline green]Enter check out date(YYYY/MM/DD): [/]");
+            var date_checkout = AnsiConsole.Ask<string>("[underline green]Enter check out date(dd/MM/yyyy): [/]");
             Console.WriteLine();
             check_out_date = EnterDate(date_checkout);
             if (check_out_date == null)
             {
                 AnsiConsole.MarkupLine("[red]Invalid input! Please enter a valid input![/]");
+            } else if (check_out_date <= check_in_date)
+            {
+                AnsiConsole.MarkupLine("[red]Check out date must be after check in date![/]");
+                check_out_date = null;
+            } else if ((check_out_date.Value - check_in_date.Value).TotalDays > 10)
+            {
+                AnsiConsole.MarkupLine("[red]The minimum stay must be 10 days![/]");
+                check_out_date = null; 
             }
         } while (check_out_date == null);
         //Confirm
-        AnsiConsole.Markup("[bold yellow]Are you sure you want to add booking? (Y/N): [/]");
+        AnsiConsole.Markup("[bold yellow]Do you want to save? (Y/N): [/]");
         string confirm = Console.ReadLine();
         switch (confirm.ToUpper())
         {
@@ -137,11 +155,33 @@ public static class BookingController
                 db.SaveChanges();
                 uI.GreenMessage("Booking added successfully!");
                 Console.WriteLine();
-                uI.PressAnyKeyToContinue();
+                AnsiConsole.Markup("[bold yellow]Do you want to add more? (Y/N): [/]");
+                confirm = Console.ReadLine();
+                if (confirm.ToUpper() == "Y")
+                {
+                    AddBooking();
+                }
+                else if (confirm.ToUpper() == "N")
+                {
+                    ManagementBooking();
+                    break;
+                }
                 break;
             case "N":
                 AnsiConsole.MarkupLine("[bold yellow]Booking not added![/]");
-                uI.PressAnyKeyToContinue();
+                Console.WriteLine();
+                AnsiConsole.Markup("[bold yellow]Do you want to add more? (Y/N): [/]");
+                confirm = Console.ReadLine();
+                if (confirm.ToUpper() == "Y")
+                {
+                    AddBooking();
+                }
+                else if (confirm.ToUpper() == "N")
+                {
+                    AnsiConsole.MarkupLine("[bold yellow]Press any key to go back![/]");
+                    Console.ReadKey();
+                }
+                // uI.PressAnyKeyToContinue();
                 break;
             default:
                 uI.RedMessage("Invalid input! Please enter a valid input!");
@@ -151,56 +191,190 @@ public static class BookingController
 
     private static void UpdateBooking()
     {
-
-    }
-
-    private static void DeleteBooking()
-    {
         ConsoleUI uI = new ConsoleUI();
-        Console.Clear();
-        uI.Title("Delete Booking");
-        int bookingId;
-        // Get booking ID
-        uI.GreenMessage("Enter booking ID to delete: ");
-        while (!int.TryParse(Console.ReadLine(), out bookingId) || bookingId <= 0)
-        {
-            AnsiConsole.MarkupLine("[bold red]Invalid input! Please enter a valid input.[/]");
-            AnsiConsole.Markup("[underline green]Enter booking ID to delete: [/]");
-        }
+        AnsiConsole.Clear();
+        // Menu.UpdateBillMenu();
+        uI.ApplicationLogoBeforeLogin();
         using var db = new HotelContext();
-        // Find booking by ID
-        var booking = db.Bookings.FirstOrDefault(r => r.BookingId == bookingId);
-        if (booking == null)
+        uI.GreenMessage("Enter Booking ID to update(Enter 0 to Exit): ");
+        int bookingId;
+        while (!int.TryParse(Console.ReadLine(), out bookingId))
         {
-            uI.RedMessage("Booking not found!");
+            AnsiConsole.MarkupLine("[bold red]Invalid input! Please enter a valid customer ID.[/]");
+            uI.GreenMessage("Enter Booking ID to update(Enter 0 to Exit): ");
+        } 
+        if (bookingId == 0)
+        {
             return;
         }
-        AnsiConsole.Markup("[bold yellow]Are you sure you want to delete this booking? (Y/N): [/]");
+        // Find the booking ID
+        var booking = db.Bookings.FirstOrDefault(u => u.BookingId == bookingId);
+        if (booking == null)
+        {
+            AnsiConsole.MarkupLine($"[bold red]Booking with ID {bookingId} not found.[/]");
+            AnsiConsole.MarkupLine("[yellow]Press any key to go back![/]");
+            Console.ReadKey();
+            return;
+        }
+        Menu.UpdateBookingMenu();
+        uI.GreenMessage("Enter your choice: ");
+        string choice = Console.ReadLine();
+        switch (choice)
+        {
+            case "1":
+                UpdateCustomerID(booking);
+                break;
+            case "2":
+                UpdateDate(booking);
+                break;
+            case "0":
+                return;
+                break;
+            
+        }
+        db.SaveChanges();
+    }
+
+    private static void UpdateCustomerID(Booking booking)
+    {
+        AnsiConsole.Clear();
+        ConsoleUI uI = new ConsoleUI();
+        uI.Title("UpdateDescription");
+        Console.WriteLine("");
+        using var db = new HotelContext();
+        // Get new customer ID
+        int customerId;
+        uI.GreenMessage("Enter new customer ID: ");
+        while (!int.TryParse(Console.ReadLine(), out customerId) || customerId <= 0)
+        {
+            AnsiConsole.MarkupLine("[underline red]Invalid input! Please enter a valid input![/]");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.White;
+            // Console.ReadKey();
+            uI.GreenMessage("Enter new customer ID: ");
+        }
+        var customer = db.Customers.FirstOrDefault(r => r.CustomerId == customerId);
+        if (customer == null)
+        {
+            uI.RedMessage("Customer not found!");
+            return;
+        }
+        // Confirm
+        AnsiConsole.MarkupLine("[bold yellow]Do you want to save? (Y/N):[/]");
         string confirm = Console.ReadLine();
         switch (confirm.ToUpper())
         {
             case "Y":
-                db.Bookings.Remove(booking);
-                db.SaveChanges();
-                uI.GreenMessage("Booking deleted successfully!");
-                Console.WriteLine();
+                booking.Customer_id = customerId;
+                uI.GreenMessage("Booking updated successfully!");
                 uI.PressAnyKeyToContinue();
                 break;
             case "N":
-                AnsiConsole.MarkupLine("[bold yellow]Booking not deleted![/]");
+                AnsiConsole.MarkupLine("[bold yellow]Booking not updated![/]");
                 uI.PressAnyKeyToContinue();
                 break;
             default:
                 uI.RedMessage("Invalid input! Please enter a valid input!");
                 break;
         }
-
     }
 
-    private static void ShowBookingList()
+    private static void UpdateDate(Booking booking)
     {
+        AnsiConsole.Clear();
+        ConsoleUI uI = new ConsoleUI();
+        uI.Title("UpdateDescription");
+        Console.WriteLine("");
+        using var db = new HotelContext();
+        DateTime? check_in_date;
+        DateTime? check_out_date;
+        // Enter check in date
+        do
+        {
+            Console.WriteLine();
+            var date_checkin = AnsiConsole.Ask<string>("[underline green]Enter new check in date(dd/MM/yyyy): [/]");
+            // Console.WriteLine();
+            check_in_date = EnterDate(date_checkin);
+            if (check_in_date == null)
+            {
+                AnsiConsole.MarkupLine("[red]Invalid input! Please enter a valid input![/]");
+            }
+        } while (check_in_date == null);
+        // Enter check out date
+        do
+        {
+            Console.WriteLine();
+            var date_checkout = AnsiConsole.Ask<string>("[underline green]Enter new check out date(dd/MM/yyyy): [/]");
+            Console.WriteLine();
+            check_out_date = EnterDate(date_checkout);
+            if (check_out_date == null)
+            {
+                AnsiConsole.MarkupLine("[red]Invalid input! Please enter a valid input![/]");
+            }
+        } while (check_out_date == null);
 
+        // Confirm
+        AnsiConsole.MarkupLine("[bold yellow]Do you want to save? (Y/N):[/]");
+        string confirm = Console.ReadLine();
+        switch (confirm.ToUpper())
+        {
+            case "Y":
+                booking.CheckInDate = (DateTime)check_in_date;
+                booking.CheckOutDate = (DateTime)check_out_date;
+                uI.GreenMessage("Booking updated successfully!");
+                uI.PressAnyKeyToContinue();
+                break;
+            case "N":
+                AnsiConsole.MarkupLine("[bold yellow]Booking not updated![/]");
+                uI.PressAnyKeyToContinue();
+                break;
+            default:
+                uI.RedMessage("Invalid input! Please enter a valid input!");
+                break;
+        }
     }
+    // private static void DeleteBooking()
+    // {
+    //     ConsoleUI uI = new ConsoleUI();
+    //     Console.Clear();
+    //     uI.Title("Delete Booking");
+    //     int bookingId;
+    //     // Get booking ID
+    //     uI.GreenMessage("Enter booking ID to delete: ");
+    //     while (!int.TryParse(Console.ReadLine(), out bookingId) || bookingId <= 0)
+    //     {
+    //         AnsiConsole.MarkupLine("[bold red]Invalid input! Please enter a valid input.[/]");
+    //         AnsiConsole.Markup("[underline green]Enter booking ID to delete: [/]");
+    //     }
+    //     using var db = new HotelContext();
+    //     // Find booking by ID
+    //     var booking = db.Bookings.FirstOrDefault(r => r.BookingId == bookingId);
+    //     if (booking == null)
+    //     {
+    //         uI.RedMessage("Booking not found!");
+    //         return;
+    //     }
+    //     AnsiConsole.Markup("[bold yellow]Do you want to save? (Y/N): [/]");
+    //     string confirm = Console.ReadLine();
+    //     switch (confirm.ToUpper())
+    //     {
+    //         case "Y":
+    //             db.Bookings.Remove(booking);
+    //             db.SaveChanges();
+    //             uI.GreenMessage("Booking deleted successfully!");
+    //             Console.WriteLine();
+    //             uI.PressAnyKeyToContinue();
+    //             break;
+    //         case "N":
+    //             AnsiConsole.MarkupLine("[bold yellow]Booking not deleted![/]");
+    //             uI.PressAnyKeyToContinue();
+    //             break;
+    //         default:
+    //             uI.RedMessage("Invalid input! Please enter a valid input!");
+    //             break;
+    //     }
+
+    // }
 
     private static void SearchBookingByCustomerID()
     {
@@ -221,10 +395,23 @@ public static class BookingController
         if (bookings.Count == 0)
         {
             AnsiConsole.MarkupLine("[bold red]There are no booking with this ID![/]");
-            AnsiConsole.Markup("[underline yellow]Press any key to go back![/]");
-            Console.ReadKey();
-            return;
-        }
+            Console.WriteLine();
+            AnsiConsole.Markup("[bold yellow]Do you want to search more? (Y/N): [/]");
+            string confirm = Console.ReadLine();
+            if (confirm.ToUpper() == "Y")
+            {
+                SearchBookingByCustomerID();
+            }
+            else if (confirm.ToUpper() == "N")
+            {
+                AnsiConsole.MarkupLine("[bold yellow]Press any key to go back![/]");
+                Console.ReadKey();
+                return;
+            }
+            // AnsiConsole.Markup("[underline yellow]Press any key to go back![/]");
+            // Console.ReadKey();
+            // return;
+        } else {
         int pageSize = 5;
         int currentPage = 1;
         int totalPages = (int)Math.Ceiling((double)bookings.Count / pageSize);
@@ -291,12 +478,13 @@ public static class BookingController
                 return;
             }
         }
+        }
     }
 
     private static DateTime? EnterDate(string date_checkin)
     {
         DateTime date;
-        if (DateTime.TryParseExact(date_checkin, "yyyy/MM/dd", null, System.Globalization.DateTimeStyles.None, out date))
+        if (DateTime.TryParseExact(date_checkin, "dd/MM/yyyy", CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out date))
         {
             // Tạo một đối tượng DateTime mới với ngày được parse và giờ là 14:00:00
             DateTime time = new DateTime(date.Year, date.Month, date.Day, 14, 0, 0);
