c7b40148ec9813384db4966348ea30539121bb97
NDKha-DEV <dinhkha2406@gmail.com>
2024-08-02 20:26:00 +0700

    Update Room.cs

--- MVC/Model/Room.cs
+++ MVC/Model/Room.cs
@@ -1,50 +1,7 @@
-// using System;
-
-// namespace Model
-// {
-//     public class Room
-//     {
-//         public int ID { get; set; }
-//         public string Name { get; set;}
-//         public string Type { get; set;}
-//         public int capacity { get; set; }
-//         public decimal PricePerNight { get; set;}
-//         public string Status { get; set;}
-//         public DateTime? CheckInDate { get; set; }
-//         public DateTime? CheckOutDate { get; set; }
-//         //public decimal PricePerNight { get; set; }
-//         public decimal Caculate_TotalPrice()
-//         {
-//             if (CheckInDate.HasValue && CheckOutDate.HasValue)
-//             {
-//                 int numberOfNights = (int)(CheckOutDate.Value - CheckInDate.Value).TotalDays;
-//                 return numberOfNights * PricePerNight;
-//             }
-//             else 
-//             {
-//                 return 0;
-//             }
-//         }
-//         // Constructor
-//         public Room()
-//         {
-            
-//             Name = "";
-//             Type = "";
-//             Status = "";
-//         }
-//         // Override ToString method to provide custom string representation
-//         // public override string ToString()
-//         // {
-//         //     return $"Room ID: {ID}, Room Name: {Name}, Room Type: {Type}, Room Price: {Price}, Room Status: {Status}";
-//         // }
-//     }
-// }
 using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-
 
 namespace Model
 {
