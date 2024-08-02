7373b3447a7fdda3c34c5091ef9876fcbc1cae57
NDKha-DEV <dinhkha2406@gmail.com>
2024-08-02 20:24:46 +0700

    Update Booking.cs

--- MVC/Model/Booking.cs
+++ MVC/Model/Booking.cs
@@ -1,31 +1,3 @@
-// using System;
-
-// namespace Model
-// {
-//     public class Booking
-//     {
-//         public int ID { get; set; }
-//         public int RoomID { get; set; }
-//         public int CustomerID { get; set; }
-//         public List<Room>? rooms { get; set; }
-//         // Constructor
-//         public Booking()
-//         {
-//             ID = BookingID;
-//             RoomID = roomID;
-//             CustomerID = customerID;
-//             CheckIn_Date = ;
-//             CheckOut_Date = ;
-            
-            
-//         }
-//         // Override ToString method to provide custom string representation
-//         // public override string ToString()
-//         // {
-//         //     return $"Booking ID: {ID}, Room ID: {RoomID}, Customer ID: {CustomerID}, Check_in Date: {CheckIn_Date}, Check_out Date: {CheckOut_Date}, Total amount: {Total_amount}";
-//         // }
-//     }
-// }
 using System;
 using System.Collections.Generic;
 
